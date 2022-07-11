ef788ce00b2f6e4384afa43376cf25f032374005
KyunghyunMoon <dpfemdhs@naver.com>
2022-07-11 14:50:06 +0900

    07.11수업

---
+++ 정규표현식.js
@@ -0,0 +1,10 @@
+// const target = 'Aa Bb 12,345 _$%&';
+// let regExp = /[\w,]+/g;
+// console.log(target.match(regExp));
+// regExp = /[w,]+/g;
+// console.log(target.match(regExp));
+
+
+
+const id = 'abc1234';
+/^[A-Za-Z0-9],{4,10}$/.test(id);
